1b48ad06c8edc9bf802d5eefde4bfc2c7553e1c0
david-pessoa <pessoa.dvlbp@gmail.com>
2025-06-18 13:41:04 -0300

    inicia front-end com bootstrap

--- station/views.py
+++ station/views.py
@@ -3,4 +3,21 @@ from django.views import View
 from .models import Agora, Hoje, Horario, ProximosDias
 
 class IndexView(View):
-    pass
+    def get(self, request):
+        agora = Agora.objects.get(pk=1)
+        hoje = Hoje.objects.get(pk=1)
+        horarios = Horario.objects.all()
+        prox_dias =  ProximosDias.objects.all()
+
+        context = {
+            "lugar": "Sao Paulo",
+            "ag": agora,
+            "hoje": hoje,
+            "horarios": horarios,
+            "prox_dias": prox_dias
+        }
+
+        return render(request, 'index.html', context)
+    
+    def post(self, request):
+        pass
