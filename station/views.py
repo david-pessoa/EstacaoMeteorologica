c750158af60b9f262635b873160d38b76a5616bb
david-pessoa <pessoa.dvlbp@gmail.com>
2025-06-19 02:19:05 -0300

    conecta API e exibe temperatura, icone do tempo e tabela com informações do vento pressão e umidade

--- station/views.py
+++ station/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render
 from django.views import View
-from .models import Agora, Hoje, Horario, ProximosDias
+import requests
 from datetime import datetime, time
+from decouple import config
 
 class IndexView(View):
     def get(self, request):
-        agora = Agora.objects.get(pk=1)
-        hoje = Hoje.objects.get(pk=1)
-        horarios = Horario.objects.all()
-        prox_dias =  ProximosDias.objects.all()
+        API_KEY = config('API_KEY')
+        local = 'Sao Paulo'
+        dias = 6
+        URL = f'http://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={local}&days={dias}&aqi=no&alerts=yes'
+        response = requests.get(URL)
+        dados = response.json()
+
+        agora = dados['current'] #Tempo agora
+        proximos_dias = dados['forecast']['forecastday']
+
+        ultima_atualizacao = agora['last_updated'].split(' ')[-1]
+        umidade = agora['humidity']
+
+        if umidade >= 60:
+            tipo_umidade = 'Alta'
+
+        elif umidade < 60 and umidade >= 30:
+            tipo_umidade = 'Moderada'
+        
+        elif umidade < 30 and umidade >= 20:
+            tipo_umidade = 'Baixa'
+        
+        else:
+            tipo_umidade = 'Muito baixa'
+        
+        angulo_vento = agora['wind_degree'] - 45 # Faz correção (a imagem do ícone já é inclinada 45º)
 
         hora_atual = datetime.now().strftime('%H:%M')
         if hora_atual > '6:00' and hora_atual < '18:00':
@@ -16,31 +39,31 @@ class IndexView(View):
         else:
             eh_dia = False
 
-        if agora.codigo_tempo == 1000 and eh_dia:
+        if agora['condition']['code'] == 1000 and eh_dia:
             icone_tempo = 'clear-day'
             texto_tempo = 'Céu limpo'
         
-        elif agora.codigo_tempo == 1000 and not eh_dia:
+        elif agora['condition']['code'] == 1000 and not eh_dia:
             icone_tempo = 'clear-night'
             texto_tempo = 'Céu limpo'
 
-        elif agora.codigo_tempo in [1003, 1006] and eh_dia:
+        elif agora['condition']['code'] in [1003, 1006] and eh_dia:
             icone_tempo = 'overcast-day'
             texto_tempo = 'Parcialmente nublado'
         
-        elif agora.codigo_tempo in [1003, 1006] and not eh_dia:
+        elif agora['condition']['code'] in [1003, 1006] and not eh_dia:
             icone_tempo = 'overcast-night'
             texto_tempo = 'Parcialmente nublado'
         
-        elif agora.codigo_tempo == 1009:
+        elif agora['condition']['code'] == 1009:
             icone_tempo = 'clouy'
             texto_tempo = 'Nublado'
         
-        elif agora.codigo_tempo in [1063, 1069, 1072] and eh_dia:
+        elif agora['condition']['code'] in [1063, 1069, 1072] and eh_dia:
             icone_tempo = 'overcast-day-drizzle'
             texto_tempo = 'Chuva'
         
-        elif agora.codigo_tempo in [1063, 1069, 1072] and not eh_dia:
+        elif agora['condition']['code'] in [1063, 1069, 1072] and not eh_dia:
             icone_tempo = 'overcast-night-drizzle'
             texto_tempo = 'Chuva'
         
@@ -51,15 +74,15 @@ class IndexView(View):
             texto_tempo = 'Tempo não configurado'
         
 
-
         context = {
-            "lugar": "Sao Paulo",
+            "lugar": local,
+            "ultima_atualizacao": ultima_atualizacao,
             "icone_tempo": icone_tempo,
             "texto_tempo": texto_tempo,
             "ag": agora,
-            "hoje": hoje,
-            "horarios": horarios,
-            "prox_dias": prox_dias
+            "prox_dias": proximos_dias,
+            "umidade": tipo_umidade,
+            "angulo_vento": angulo_vento,
         }
 
         return render(request, 'index.html', context)
