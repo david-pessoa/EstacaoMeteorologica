ab388f86d23ccfba734971315bb09dc73065ef80
david-pessoa <pessoa.dvlbp@gmail.com>
2025-06-19 21:26:48 -0300

    troca database

--- project/settings.py
+++ project/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
