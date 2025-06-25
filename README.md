cccb1281fc8994573ad53b4016de48bd9d79c951
david-pessoa <pessoa.dvlbp@gmail.com>
2025-06-24 21:02:11 -0300

    atualiza staticfiles e README

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
-# EstacaoMeteorologica
+# Endereço na Internet
+ * http://3.149.124.111/
+
+# Passo a passo para configuração do ambiente virtual
+
+## Pré-requisitos:
+ * Python 3.12 ou superior
+ * Possuir uma chave para a API da WeatherAPI (é possível utilizar o plano gratuito)
+
+
+## 1) Clone o repositório
+```
+git clone https://github.com/david-pessoa/EstacaoMeteorologica.git
+```
+
+## 2) Entre no diretório raíz do projeto, instale o ambiente virtual venv e ative-o
+```
+python3 -m venv venv
+```
+
+### No Linux ou MacOS
+```
+source venv/bin/activate
+```
+
+### No Windows powershell
+```
+source venv/Scripts/Activate.ps1
+```
+
+### No Windows cmd
+```
+source venv/Scripts/Activate.bat
+```
+
+## 3) Instale todos os pacotes necessários
+```
+pip install -r requirements.txt
+```
+
+## 4) Crie um arquivo .env na mesma pasta e gere uma SECRET_KEY 
+```
+python
+from django.core.management.utils import get_random_secret_key
+print(get_random_secret_key())
+```
+
+## 5) Agora preencha o arquivo .env com as informações a seguir substituindo os respectivos valores
+```
+SECRET_KEY='sua senha secreta'
+
+API_KEY="chave da API"
+
+DEBUG=True
+```
+
+## 6) Crie um novo super usuário
+```
+python manage.py createsuperuser
+```
+
+## 7) Agora rode o migrate e o servidor
+```
+python manage.py migrate
+python manage.py runserver
+```
+
+# Configurando o Docker no ambiente de produção
+## 0) Se você está apenas atualizando o código na instância, rode
+```
+sudo docker-compose down --volumes --remove-orphans
+sudo docker-compose up -d
+```
+
+## 1) Se você estiver criando as imagens do zero, prepare-as com docker compose
+```
+sudo docker-compose build --no-cache
+```
+
+## 2) Rode os containers em segundo plano
+```
+sudo docker-compose up -d
+```
+
+### Se esse passo falhar, digite os comandos a seguir e tente de novo
+```
+sudo docker-compose down --volumes --remove-orphans
+sudo docker image prune -a
+```
+
+## 3) Verifique os containers
+```
+sudo docker ps
+```
--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,16 +1,109 @@
-const ctx = document.getElementById('graficoTemperatura');
+import Autocomplete from './autocomplete.js';
 
-const horarios = Array.from({length: 24}, (_, i) => {
-    return `${i.toString().padStart(2, '0')}:00`; // Horários das 0:00 até às 23:00
+const LastUpdate = document.getElementById('last_update') //tag onde fica o texto que informa a ultima atualização
+const ctx = document.getElementById('graficoTemperatura');
+const AutocompleteDataList = document.getElementById("AutocompleteDataList");
+const SwitchThemeButton = document.getElementById('flexSwitchCheckDefault');
+
+  const opts = {
+    onSelectItem: console.log,
+  };
+
+new Autocomplete(AutocompleteDataList, opts);
+
+AutocompleteDataList.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault(); // impede que o autocomplete ou o formulário façam outra ação
+
+        const cidade = AutocompleteDataList.value.trim();
+
+        if (cidade) {
+            window.location.href = `/?local=${encodeURIComponent(cidade)}`;
+        }
+    }
 });
 
-let temperaturas = [];
+document.addEventListener('click', function(event) {
+    const link = event.target.closest('.dropdown-item');
+    if (link) {
+        event.preventDefault();  // Impede que o autocomplete só preencha
 
+        const cidade = link.getAttribute('data-value');
+        if (cidade) {
+            window.location.href = `/?local=${encodeURIComponent(cidade)}`;
+        }
+    }
+});
+
+SwitchThemeButton.addEventListener('click', function(event) {
+    const body = document.getElementsByTagName('body')[0]
+    const boxes = Array.from(document.getElementsByClassName('bg-dark'));
+    const tables = Array.from(document.getElementsByClassName('table-dark'));
+    const titles = Array.from(document.getElementsByTagName('h4', 'h5'));
+    const subtitles = Array.from(document.getElementsByTagName('h5'));
+
+    if (SwitchThemeButton.checked)
+    {
+        body.style.backgroundColor = '#b2f7d8';
+        body.style.color = '#000';
+        
+        titles.forEach(el => {
+            el.style.color = '#FFF';
+        });
+
+        subtitles.forEach(el => {
+            el.style.color = '#FFF';
+        });
+
+        boxes.forEach(el => {
+            el.style.setProperty('background-color', 'rgba(26, 161, 121, 1)', 'important');
+        });
+
+        tables.forEach(el => {
+            el.style.setProperty('--bs-table-bg', '#1AA179');
+        });
+    }
+    else
+    {
+        body.style.backgroundColor = '';
+        body.style.color = ''
+
+        boxes.forEach(el => {
+            el.style.setProperty('background-color', 'rgba(33, 37, 41, 1)', 'important');
+        });
+
+        tables.forEach(el => {
+            el.style.setProperty('--bs-table-bg', '#212529');
+        });
+    }
+});
+
+const data = new Date(ultima_atualizacao); //Obtém data e converte para o fuso horário do Usuário
+
+const horas = data.getHours().toString().padStart(2, '0');
+const minutos = data.getMinutes().toString().padStart(2, '0');
+
+const hoje = new Date();
+const ehHoje = data.getDate() === hoje.getDate() &&
+                data.getMonth() === hoje.getMonth() &&
+                data.getFullYear() === hoje.getFullYear();
+
+const texto = ehHoje ? `Última atualização: Hoje às ${horas}:${minutos}` : 
+    `${data.getDate()} de ${data.toLocaleString('pt-BR', { month: 'long' })} às ${horas}:${minutos}`;
+
+LastUpdate.textContent = texto
+
+let temperaturas = [];
+let temp;
 for (let i = 0; i < 24; i++)
 {
     temp = Math.round(previsao_hoje.hour[i].temp_c);
     temperaturas.push(temp);
 }
+
+const horarios = Array.from({length: 24}, (_, i) => {
+    return `${i.toString().padStart(2, '0')}:00`; // Horários das 0:00 até às 23:00
+});
 
 new Chart(ctx, {
     type: 'line',
